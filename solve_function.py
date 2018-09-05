45bd17a1eadc3f415191bb6ff37fd8babc463300
william0418 <42887704+william0418@users.noreply.github.com>
2018-09-05 17:39:26 +0800

    Add files via upload

--- solve_function.py
+++ solve_function.py
@@ -15,12 +15,19 @@ print(y)
 img = cv2.imread('', cv2.IMREAD_GRAYSCALE)
 
 arr = np.zeros(2,(img.shape[0]+img.shape[1])*2)
+arr_xy = np.zeros(5,(img.shape[0]+img.shape[1])*2)
 
 for i in range(img.shape[0]):
     for j in range(im.shape[1]):
         if im[i,j] != 0:
             arr[0,i] = i
             arr[1,i] = j
+            arr_xy[0,i] = 1
+            arr_xy[1,i] = pow(arr[0,i],1)
+            arr_xy[2,i] = pow(arr[0,i],2)
+            arr_xy[3,i] = pow(arr[1,i],1)
+            arr_xy[4,i] = pow(arr[1,i],2)
+            arr_xy[5,i] = arr[0,i]*arr[1,i]
             break()
 
 for i in range(img.shape[0]):
@@ -43,3 +50,4 @@ for j in range(img.shape[1]):
             arr[0,2*img.shape[0]+img.shape[1]+j] = img.shape[0]-i
             arr[1,2*img.shape[0]+img.shape[1]+j] = j
             break()
+print(arr)
